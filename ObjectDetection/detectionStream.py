3707d4c76aab3f8526cfd2071525b846b6052a59
Pablo Santiago <pablo.santiago6@upr.edu>
2020-03-31 19:22:47 -0400

    initial files

--- ObjectDetection/detectionStream.py
+++ ObjectDetection/detectionStream.py
@@ -15,10 +15,14 @@ import imutils
 import time
 import cv2
 import numpy as np
+import subprocess
+import jetson.utils 
+import jetson.inference
 
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful for multiple browsers/tabs
 # are viewing tthe stream)
+
 outputFrame = None
 lock = threading.Lock()
 fps = FPS().start()
@@ -43,104 +47,50 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 # initialize the video stream and allow the camera sensor to
 # warmup
 #vs = VideoStream(usePiCamera=1).start()
-vs = VideoStream(src=0).start()
+vs = jetson.utils.gstCamera(1280 , 720 , "0")
+
+
 time.sleep(2.0)
 
 @app.route("/")
 def index():
     # return the rendered template
-    return render_template("index.html")
+    text = open('readings.txt' , 'r+')
+    content = text.read()
+    text.close
+    return render_template("index.html", text = content)
 
 def objectDetection(frame_count):
 
     print("-------------------------Entered Object Detection--------------")
     global vs ,  outputFrame ,lock , fpsStarted , oldFPS , frameID
     # model
-    net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+    net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 
     while True:
-        # with lock:
-        # grab the frame from the threaded video stream and resize it
-        # to have a maximum width of 400 pixels
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
+        
+        #Capture from camera
+        frame, w , h  = vs.CaptureRGBA(zeroCopy=1)
 
-
-        # grab the frame dimensions and convert it to a blob
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-            0.007843, (300, 300), 127.5)
-
-        # pass the blob through the network and obtain the detections and
-        # predictions
-        net.setInput(blob)
-        detections = net.forward()
+        #Pass img to net 
+        detections = net.Detect(frame , w, h)
 
         timestamp = datetime.datetime.now()
 
-        # loop over the detections
-        for i in np.arange(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with
-            # the prediction
-            confidence = detections[0, 0, i, 2]
-
-            # filter out weak detections by ensuring the `confidence` is
-            # greater than the minimum confidence
-            if confidence > args["confidence"]:
-                # extract the index of the class label from the
-                # `detections`, then compute the (x, y)-coordinates of
-                # the bounding box for the object
-                idx = int(detections[0, 0, i, 1])
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
-
-                # draw the prediction on the frame
-                label = "{}: {:.2f}%".format(CLASSES[idx],
-                    confidence * 100)
-                cv2.rectangle(frame, (startX, startY), (endX, endY),
-                    COLORS[idx], 2)
-                y = startY - 15 if startY - 15 > 15 else startY + 15
-                cv2.putText(frame, label, (startX, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-
-        elapsedTIME = timestamp - fpsStarted
-        fpsFormat= "FPS: {}"
-        if elapsedTIME.seconds >= 1:
-            fps.stop()
-            cv2.putText(frame,fpsFormat.format( fps.fps() ),
-                (10 , 50),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (0,0,0),
-                2)
-            oldFPS = fps.fps()
-            fpsStarted = fps.start()._start
-            fps._numFrames = 0
-        else:
-            cv2.putText(frame,fpsFormat.format(oldFPS),
-                (10 , 50),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (0,0,0),
-                2)
-        cv2.putText(frame, str(timestamp),
-            (0 , h -10),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0,0,0),
-            2)
-        fps.update()
-
-        # show the output frame
-        # cv2.imshow("Frame", frame)
-
+        frame = jetson.utils.cudaToNumpy(frame , w, h , 4)
+        cv2.putText(frame ,"Object Detection Network {:.0f} FPS".format(net.GetNetworkFPS()), 
+        (10 , 500), 
+        cv2.FONT_HERSHEY_SIMPLEX, 
+        1,
+        (255,255,255),
+        2)
+       
         with lock:
             # FrameTimeFormat = "Time spent on Frame {} : {}us "
             # frameID = frameID + 1
             # print(FrameTimeFormat.format(frameID , (datetime.datetime.now() - timestamp).microseconds ))
-            outputFrame = frame.copy()
-
-
+            
+            outputFrame = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
 
 
 
@@ -214,4 +164,3 @@ if __name__ == '__main__':
     print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
     # do a bit of cleanup
-    vs.stop()
